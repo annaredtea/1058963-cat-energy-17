9b4cb8244ea32b67635768961537f6a0c9d677f2
annaredtea <50323998+annaredtea@users.noreply.github.com>
2019-09-01 21:45:28 +0300

    no hope

--- source/js/navigation-open-close.js
+++ source/js/navigation-open-close.js
@@ -1,8 +1,9 @@
-var toggleMenu = document.querySelector(".main-navigation__toggle");
+var toogleMenu = document.querySelector(".main-navigation__toggle");
+var mainNav = document.querySelector(".main-navigation__list");
 
-var navigation = document.querySelector(".main-navigation__list");
+toogleMenu.classList.remove(".main-navigation__toggle--no-js");
 
-toggleMenu.addEventListener("click", function() {
-  toggleMenu.classList.toggle(".main-navigation__toggle--close");
-  navigation.classList.toggle("main-navigation__list--open");
+toogleMenu.addEventListener("click", function () {
+  toogleMenu.classList.toggle("main-navigation__toggle--open");
+  mainNav.classList.toggle("main-navigation__list--open");
 });
